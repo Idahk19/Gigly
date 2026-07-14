e337a8df28ee9dc00486af83844a7032e26a5849
Idahk19 <idahkarwitha9@gmail.com>
2026-07-14 15:49:32 +0300

    Import Register and Login to the App.jsx and have them as routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { LogIn } from "lucide-react";
 import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
     <div>
       <Router>
-         <Home />
+       <Route path = '/' element = {<Home />}  />
+       <Route path='/signup' element={<Register/>} />
+       <Route path='/login' element={<Login/>} />
       </Router> 
     </div>
   );
