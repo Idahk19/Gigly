12ae8a505fe104b72e9c1f90207b37c76970527b
Idahk19 <idahkarwitha9@gmail.com>
2026-07-14 12:28:40 +0300

    Import the Hero in the Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import Hero from '../components/Hero'
 
 function Home() {
   return (
     <div>
       <Navbar />
+      <Hero />
       <Footer />
     </div>
   )
