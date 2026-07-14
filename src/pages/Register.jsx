8d92640de2c5d6b3927e527b22d32d318e895e31
Idahk19 <idahkarwitha9@gmail.com>
2026-07-14 22:37:17 +0300

    Initialize the registration page - the login card

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 function Register() {
   return (
-    <div>
-      
+    <div className="max-w-5xl mx-auto rounded-3xl overflow-hidden bg-white/20 backdrop-blur-xl border border-white/30 shadow-2xl">
+  <div className="grid md:grid-cols-2">
+    <div className="bg-gradient-to-br from-indigo-600 to-indigo-700 text-white p-10">
+        
+    </div>
+     </div>
+
+    <div className="bg-white/40 p-10">
+    </div>
     </div>
   )
 }
