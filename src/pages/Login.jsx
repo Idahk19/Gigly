67c39601e45341939db33c95b9ddba95cd3f15bd
Idahk19 <idahkarwitha9@gmail.com>
2026-07-15 00:23:46 +0300

    Increase the margin top, align the eye icon with the place holder, change the paths to login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -15,7 +15,7 @@ function Register() {
               className="w-full h-full object-cover"
             />
           </div>
-    <div className="max-w-5xl mx-auto mt-5 rounded-3xl overflow-hidden bg-white/20 backdrop-blur-xl border border-white/30 shadow-2xl">
+    <div className="max-w-5xl mx-auto mt-10 rounded-3xl overflow-hidden bg-white/20 backdrop-blur-xl border border-white/30 shadow-2xl">
  <div className="grid lg:grid-cols-2 gap-8 items-center">
     <div className="  p-10">
     <h1 className="text-6xl font-bold leading-tight bg-gradient-to-r from-indigo-600 via-violet-600 to-emerald-500 bg-clip-text text-transparent">
@@ -50,6 +50,9 @@ function Register() {
   </div>
 
  <div className="relative">
+    <label className="block mb-2 text-sm font-medium text-slate-700">
+      Password
+    </label>
     <input
       type={showPassword ? "text" : "password"}
       placeholder="Enter your password"
@@ -59,7 +62,7 @@ function Register() {
     <button
       type="button"
       onClick={() => setShowPassword(!showPassword)}
-      className="absolute right-3 top-1/2 -translate-y-1/2 text-slate-500 hover:text-indigo-600"
+      className="absolute right-3 top-2/3 -translate-y-1/2 text-slate-500 hover:text-indigo-600"
     >
       {showPassword ? <EyeOff size={18} /> : <Eye size={18} />}
     </button>
@@ -77,10 +80,10 @@ function Register() {
   <p className="text-center text-slate-600">
     Don't have an account ?{" "}
     <a
-      href="/login"
+      href="/signup"
       className="font-semibold text-indigo-600 hover:text-indigo-700"
     >
-      Login
+      Sign Up
     </a>
   </p>
 </form>
