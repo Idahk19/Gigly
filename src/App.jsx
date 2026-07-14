2370b786f985dd311f2f6e490593f6f334aa85a9
Idahk19 <idahkarwitha9@gmail.com>
2026-07-15 00:37:22 +0300

    Import the pages and have them wrapped inside the routes on app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import Home from "./pages/Home";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Privacy from "./pages/PrivacyPolicy";
+import TermsConditions from "./pages/termsconditions"
+import Support from "./pages/Support"
+import FAQ from "./pages/FAQ"
+import About from "./pages/About"
 
 function App() {
   return (
@@ -12,6 +17,11 @@ function App() {
         <Route path = '/' element = {<Home />}  />
        <Route path='/signup' element={<Register/>} />
        <Route path='/login' element={<Login/>} />
+       <Route path='/privacypolicy' element={<PrivacyPolicy/>} />
+       <Route path='/termsconditions' element={<TermsConditions />} />
+       <Route path='/support' element={<Support/>} />
+       <Route path='/faq' element={<FAQ/>} />
+       <Route path='/about' element={<About/>} />
         </Routes>
       </Router> 
     </div>
