848da0c501d530751a1425b45a3b3d19e1dc3afe
Idahk19 <idahkarwitha9@gmail.com>
2026-07-14 10:59:17 +0300

    Import Icons needed for the footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+ import React from 'react'
+ import {
+  FolderKanban,
+  Facebook,
+  Instagram,
+  Linkedin,
+  Twitter,
+  Mail,
+} from "lucide-react";
+ 
+ function Footer() {
+   return (
+     <div>
+       
+     </div>
+   )
+ }
+ 
+ export default Footer
+ 
